88decdec914840d9f95167cedec151a818059c75
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-26 16:00:12 +0530

    Added: Function are check NO play Ladder or Snake.

--- src/com/blz/game/SnakeAndLadder.java
+++ src/com/blz/game/SnakeAndLadder.java
@@ -1,10 +1,22 @@
 package com.blz.game;
 
 public class SnakeAndLadder {
+    public static final int START_POSITION =0;
+    public static final int IS_SNAKE =1;
+    public static final int IS_LADDER =2;
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game");
-        int startPosition =0;
+        int currentPosition =0;
          int diceNumber = (int) ((Math.random() * 10 % 6) +1);
-        System.out.println("dice Number:"+ diceNumber);
+        int option = (int)  ((Math.random() * 10 % 3) +1);
+
+        if (option == IS_SNAKE){
+             currentPosition -= diceNumber;
+        }else if(option == IS_LADDER) {
+            currentPosition += diceNumber;
+        }
+        else {
+            System.out.println("NO Play");
+        }
     }
 }
