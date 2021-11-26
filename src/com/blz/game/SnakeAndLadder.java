3b76dbc60072dae35e0dca2f3d578c5065f70637
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-26 14:16:52 +0530

    Added: Player rolls the die to get value between 1 to 6.

--- src/com/blz/game/SnakeAndLadder.java
+++ src/com/blz/game/SnakeAndLadder.java
@@ -4,5 +4,7 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game");
         int startPosition =0;
+         int diceNumber = (int) ((Math.random() * 10 % 6) +1);
+        System.out.println("dice Number:"+ diceNumber);
     }
 }
