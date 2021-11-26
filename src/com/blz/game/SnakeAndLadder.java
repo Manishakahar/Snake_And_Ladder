c4cd803a84b7e899360b5a08cd9f4a22dbb08bdb
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-26 13:59:00 +0530

    Added:Initialized Player Position.

--- src/com/blz/game/SnakeAndLadder.java
+++ src/com/blz/game/SnakeAndLadder.java
@@ -3,5 +3,6 @@ package com.blz.game;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game");
+        int startPosition =0;
     }
 }
