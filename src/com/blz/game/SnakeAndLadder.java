ac5965aecd1bb840dd75bd8790827cd23519eacc
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-26 21:16:00 +0530

    Added: Check Player Exact Winning Position is 100 or Not

--- src/com/blz/game/SnakeAndLadder.java
+++ src/com/blz/game/SnakeAndLadder.java
@@ -1,36 +1,45 @@
 package com.blz.game;
 
 public class SnakeAndLadder {
-    public static final int START_POSITION =0;
-    public static final int IS_SNAKE =1;
-    public static final int IS_LADDER =2;
-    public static final int NO_PLAY =0;
+    public static final int START_POSITION = 0;
+    public static final int IS_SNAKE = 1;
+    public static final int IS_LADDER = 2;
+    public static final int NO_PLAY = 0;
 
-    public static void setPlayerMoves(){
-        int playerPosition=0;
-        while(playerPosition<100){
-            int diceNumber = (int) ((Math.random() * 10 % 6) + 1);
+    static int playerPosition = 0;
+    static int diceNumber = 0;
+
+    public static void setPlayerMoves() {
+        while (playerPosition < 100) {
+            diceNumber = (int) ((Math.random() * 10 % 6) + 1);
             int option = (int) ((Math.random() * 10 % 3) + 1);
 
             if (option == IS_SNAKE) {
                 playerPosition -= diceNumber;
             }
-            if(option == IS_LADDER) {
+             else if (option == IS_LADDER) {
                 playerPosition += diceNumber;
             }
-            if(option==NO_PLAY) {
+            else if (option == NO_PLAY) {
                 System.out.println("NO Play");
             }
-            if(playerPosition<START_POSITION){
-                playerPosition=START_POSITION;
-            }
 
-            System.out.println("Player position: "+playerPosition);
+            if (playerPosition < START_POSITION) {
+                playerPosition = START_POSITION;
+            }else if(playerPosition>100){
+                playerPosition -= diceNumber;
+            }
+            System.out.println("Player position: " + playerPosition);
         }
     }
+     public static void  playUntilWin(){
+        while (playerPosition != 100){
+            setPlayerMoves();
+        }
+     }
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game");
-        setPlayerMoves();
+        playUntilWin();
         System.out.println("Player has won");
     }
 }
